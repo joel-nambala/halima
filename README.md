9c9f182670a08a3e7d8f5a5905592909c5beeb11
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-04-30 17:23:00 +0300

    Responsiveness and dynamic

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# halima
-Creating awareness about courses that Halima offers
+# Halima courses
+
+This website creates awareness about various courses that Hlima offers.
+
+## Available scripts
+
+The scripts that I used to run this website are
+
+### `npm run start`
+
+It starts the website in the development enviroment. Basically creates a local server where my website runs on during development.
+
+### `npm run build`
+
+The script buils the website ready for shipment. It complies the code, cancatenates the stylesheets I used and compresses them into a minified file.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+///////////////////////////////////////////////////
+// Copyright year
+const copyrightYear = document.querySelector('.copyright-year');
+
+/**
+ * @argument no arguements
+ * @author Joel Nambala
+ * @description changes the copyright year dynamically
+ */
+const changeCopyrightYear = function () {
+  // 1. Get the date object
+  const today = new Date();
+
+  // 2. Get the current year
+  const currentYear = today.getFullYear();
+
+  // 3. Display the current year to the UI
+  copyrightYear.textContent = currentYear;
+};
+
+changeCopyrightYear();
+
+////////////////////////////////////////
+// Sticky navigation
+const navigation = document.querySelector('.navigation');
+
+/**
+ * @author Joel Nambala
+ * @param No parameters
+ * @description Adds a fixed navigation class to the nav on scroll
+ */
+const fixedNav = function () {
+  // 1. Get the scroll height of the document on scroll
+  const scrollHeight = window.scrollY;
+
+  // 2. Get the height of the navigation bar
+  const navHeight = navigation.getBoundingClientRect().height;
+
+  // 3. Add and remove the sticky class based on different consitions
+  if (scrollHeight > navHeight) navigation.classList.add('sticky');
+  else navigation.classList.remove('sticky');
+};
+
+window.addEventListener('scroll', fixedNav);
+
+////////////////////////////////////////////
+// Responsive navigation
+const navToggle = document.querySelector('.nav__bars');
+const linksContainer = document.querySelector('.links__container');
+const navList = document.querySelector('.nav__list');
+
+/**
+ * @author Joel Nambala
+ * @argument No arguments
+ * @description Resposive navigation bar
+ */
+navToggle.addEventListener('click', function () {
+  // 1. Get the height of the navigation bar
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const listHeight = navList.getBoundingClientRect().height;
+
+  // 2. Add the height dynamically
+  if (containerHeight === 0) linksContainer.style.height = `${listHeight}px`;
+  else linksContainer.style.height = 0;
+});
+
+linksContainer.addEventListener('click', function () {
+  linksContainer.style.height = 0;
+});
